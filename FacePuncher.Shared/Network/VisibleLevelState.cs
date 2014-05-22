2258adad385629d53302be68457688475ba2f999
Tamschi <MailTamschi@googlemail.com>
2014-05-22 07:25:10 +0200

    Small commit to change PC

---
+++ FacePuncher.Shared/Network/VisibleLevelState.cs
@@ -0,0 +1,24 @@
+﻿using FacePuncher.Geometry;
+using FacePuncher.Graphics;
+using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacePuncher.Network
+{
+    [ProtoContract]
+    public class VisibleLevelState
+    {
+        [ProtoMember(1)]
+        public Rectangle Rect { get; private set; }
+        [ProtoMember(2)]
+        private TileAppearance[][] _appearances;
+        public TileAppearance this[int relX, int relY]
+        {
+            get { return _appearances[relX][relY]; }
+        }
+    }
+}
